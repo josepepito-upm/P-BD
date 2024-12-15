d2a11a99a9014b8aeddd6bc2c98348fffd7b11eb
PabloPlaza30 <pablo.plazagonzalo@gmail.com>
2024-12-15 23:10:51 +0100

    he vuelto a subir la consulta ñ con los cambios realizados y ya corregida

--- E2/Procedure_Apartado_ñ.sql
+++ E2/Procedure_Apartado_ñ.sql
@@ -1,13 +1,15 @@
+DROP PROCEDURE IF EXISTS updateVip;
+
 DELIMITER //
 
-CREATE PROCEDURE curdemo()
+CREATE PROCEDURE updateVip()
 BEGIN 
 	DECLARE done INT DEFAULT FALSE;
 	DECLARE gasto DECIMAL(10, 2);
 	DECLARE pasa VARCHAR(7); 
     
 	DECLARE cur CURSOR FOR 
-		SELECT SUM(cantidad), pasajero
+		SELECT pasajero, SUM(cantidad)
 		FROM gastos g JOIN pasajeros p ON p.id = g.pasajero
 		WHERE vip = 0
 		GROUP BY id; 
@@ -24,9 +26,13 @@ BEGIN
 		END IF;
 		
 		IF (gasto > 5000) THEN
+			SET SQL_SAFE_UPDATES = 0;
+            
 			UPDATE pasajeros
 			SET vip = 1
 			WHERE id = pasa;
+            
+            SET SQL_SAFE_UPDATES = 1;
 		END IF;
 	
 	END LOOP;
