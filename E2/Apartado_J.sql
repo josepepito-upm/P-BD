04ab0701f78f30cdcfe4af96749a1733ed40d5ca
lolteam00 <david.sanchez180305@gmail.com>
2024-12-16 13:09:54 +0100

    Update Apartado_J.sql

--- E2/Apartado_J.sql
+++ E2/Apartado_J.sql
@@ -1,14 +1,14 @@
-select num_entretenidos.planeta_natal, num_entretenidos.sistema_natal, tot_pasajeros.pasajeros_nacidos, num_entretenidos.pasajeros_entretenidos
-from (select planeta_natal, sistema_natal, count(*) AS pasajeros_entretenidos
-		from pasajeros
-		where id IN( select pasajero
-					from gastos
-					group by pasajero
-					having Count(*) >= 4 )
-		group by planeta_natal, sistema_natal) num_entretenidos
-join (select planeta_natal, sistema_natal, count(*) as pasajeros_nacidos
-			from pasajeros
-			group by planeta_natal, sistema_natal) tot_pasajeros
+SELECT num_entretenidos.planeta_natal, num_entretenidos.sistema_natal, tot_pasajeros.pasajeros_nacidos, num_entretenidos.pasajeros_entretenidos
+FROM (SELECT planeta_natal, sistema_natal, COUNT(*) AS pasajeros_entretenidos
+		FROM pasajeros
+		WHERE id IN( SELECT pasajero
+					FROM gastos
+					GROUP BY pasajero
+					HAVING COUNT(*) >= 4 )
+		GROUP BY planeta_natal, sistema_natal) num_entretenidos
+JOIN (SELECT planeta_natal, sistema_natal, COUNT(*) AS pasajeros_nacidos
+			FROM pasajeros
+			GROUP BY planeta_natal, sistema_natal) tot_pasajeros
 ON num_entretenidos.planeta_natal = tot_pasajeros.planeta_natal 
 AND num_entretenidos.sistema_natal = tot_pasajeros.sistema_natal
-where num_entretenidos.pasajeros_entretenidos > tot_pasajeros.pasajeros_nacidos * 0.25
+WHERE num_entretenidos.pasajeros_entretenidos > tot_pasajeros.pasajeros_nacidos * 0.25
