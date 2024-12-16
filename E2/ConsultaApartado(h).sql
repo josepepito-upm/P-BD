5a67b1c49845fa76cedf1f7f0b1580fa3f92b7f5
ArrobaAlex <al.rosado@alumnos.upm.es>
2024-12-16 14:03:15 +0100

    Delete E2 directory

--- E2/ConsultaApartado(h).sql
+++
@@ -1,8 +0,0 @@
-SELECT pl.nombre, pl.sistema
-FROM pasajeros pa JOIN planetas pl ON pa.planeta_destino = pl.nombre
-WHERE pa.planeta_destino NOT IN ( SELECT pa1.planeta_destino
-									FROM pasajeros pa1 
-									WHERE pa1.vip = 1)
-AND pa.planeta_destino IN ( SELECT pa1.planeta_natal
-							FROM pasajeros pa1 
-                            WHERE edad >= 70 )
