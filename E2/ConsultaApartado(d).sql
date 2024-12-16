1219183bac563b48ea7bffde61e3690f107e8a13
ArrobaAlex <al.rosado@alumnos.upm.es>
2024-12-16 12:58:30 +0100

    Update ConsultaApartado(d).sql

--- E2/ConsultaApartado(d).sql
+++ E2/ConsultaApartado(d).sql
@@ -4,12 +4,4 @@ SELECT e.nombre, SUM(g.cantidad), CASE
 								END AS tipo
 FROM entretenimientos e JOIN gastos g ON e.id = g.entretenimiento
 						JOIN pasajeros p ON g.pasajero = p.id
-GROUP BY e.nombre, p.vip
-UNION
-SELECT e.nombre, SUM(g.cantidad), CASE 
-								WHEN p.vip = 1 THEN 'VIP'
-								ELSE 'NO VIP'
-								END AS tipo
-FROM entretenimientos e JOIN gastos g ON e.id = g.entretenimiento
-						JOIN pasajeros p ON g.pasajero = p.id
 GROUP BY e.nombre, p.vip;
